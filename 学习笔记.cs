adb60d1a5284cc953da5eb2a13c6edf5a0f8d6ae
sqlfans <49314080+sqlfans@users.noreply.github.com>
2019-04-22 17:51:01 +0800

    Update 学习笔记.cs

--- 学习笔记.cs
+++ 学习笔记.cs
@@ -1,4 +1,4 @@
-//交换变量练习
+//两个变量交换值练习
 int x, y;
 int temp;
 x = 0;
@@ -23,6 +23,31 @@ Console.WriteLine("闰年判断练习：");
 Console.WriteLine("请输入年份：");
 int year = Convert.ToInt32(Console.ReadLine());
 bool nonlLeapYear, LeapYear;//是否是闰年，平年
-LeapYear = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-nonlLeapYear = !LeapYear;
-Console.WriteLine("年份{0}是否为闰年判断结果： {1}", year, LeapYear);
+isLeapYear = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+notislLeapYear = !LeapYear;
+Console.WriteLine("年份{0}是否为闰年判断结果： {1}", isLeapYear);
+//自增自减练习
+int x;
+x = 0;
+Console.WriteLine("{0}", x);//0
+++x;
+Console.WriteLine("{0}", x);//1
+Console.WriteLine("{0}", ++x);//2
+++x;
+Console.WriteLine("{0}", x);//3
+Console.WriteLine("{0}", ++x);//4
+int y;
+y = 0;
+Console.WriteLine("{0}", y);//0
+y++;
+Console.WriteLine("{0}", y);//1
+Console.WriteLine("{0}", y++);//1
+y++;
+Console.WriteLine("{0}", y);//3
+Console.WriteLine("{0}", y++);//3
+int z = 0;
+Console.WriteLine(z.ToString());//0
+Console.WriteLine((--z).ToString());//-1
+Console.WriteLine((--z).ToString());//-2
+Console.WriteLine((z--).ToString());//-2
+Console.WriteLine((z--).ToString());//-3
